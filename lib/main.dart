25c024920e9083e18a3abb9f49696869bebd0311
Zaw Min Tun <zawmintun.tp@gmail.com>
2022-12-01 21:57:56 +0630

    update project setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'Providers/great_places.dart';
+import 'screens/places_list_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.amber,
+    return ChangeNotifierProvider.value(
+      value: GreatePlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
       ),
-      home: ...,
     );
   }
 }
